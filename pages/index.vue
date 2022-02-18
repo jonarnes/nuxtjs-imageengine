703122136cefb6210caf5fa989bbac75ae8989d5
Jon Arne S <seteras@gmail.com>
2022-02-18 11:57:19 +0100

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <main>
+    <!-- https://image.nuxtjs.org/components/nuxt-picture -->
+    <!-- https://image.nuxtjs.org/components/nuxt-img -->
+    <figure>
+      <nuxt-img
+        provider="imageengine"
+        src="/images/mountains.jpg"
+        width="3750"
+        height="2500"
+        sizes="xs:200px md:500px lg:1024"
+      />
+    </figure>
+  </main>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
